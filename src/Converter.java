640d5fa60345184cc8bcbb6bbf5c78fa96cc9f4a
paulofranklins <paulofranklinc@gmail.com>
2023-06-30 23:13:19 -0700

    This commit refactors the ageConverter() method in the Converter class to improve code structure and error handling.

--- src/Converter.java
+++ src/Converter.java
@@ -4,30 +4,52 @@ import java.util.Scanner;
 public class Converter {
     private int dogAge;
     private int humanAge;
-    private boolean invalid = false;
 
     public void ageConverter() {
         Scanner scanner = new Scanner(System.in);
-        do {
+        while (true) {
             try {
                 System.out.println("Type your dog age");
-                this.dogAge = scanner.nextInt();
-                if (this.dogAge < 0) {
+                int age = scanner.nextInt();
+                if (age < 0) {
                     throw new IllegalArgumentException("Negative number's not allowed");
                 }
-                invalid = true;
+                this.setDogAge(age);
+                break;
             } catch (InputMismatchException e) {
                 System.out.println("Invalid input make sure you enter a valid input");
                 scanner.nextLine();
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
-
-        } while (!invalid);
+        }
         scanner.close();
-        this.humanAge = this.dogAge * 7;
-        System.out.println("Dogs Age: " + this.dogAge);
-        System.out.println("Dogs Age Converted To Human: " + this.humanAge);
+        ageCalc();
+        printAge();
     }
 
+    public void ageCalc() {
+        this.setHumanAge(this.getDogAge() * 7);
+    }
+
+    public void printAge() {
+        System.out.println("Dogs Age: " + this.getDogAge());
+        System.out.println("Dogs Age Converted To Human: " + this.getHumanAge());
+    }
+
+    public int getDogAge() {
+        return dogAge;
+    }
+
+    public void setDogAge(int dogAge) {
+        this.dogAge = dogAge;
+    }
+
+    public int getHumanAge() {
+        return humanAge;
+    }
+
+    public void setHumanAge(int humanAge) {
+        this.humanAge = humanAge;
+    }
 }
